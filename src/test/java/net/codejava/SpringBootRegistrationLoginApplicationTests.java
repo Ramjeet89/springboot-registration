e98abfb9727ccf343fa4d529164d77fe71c926c3
Ramjeet89 <ramjeet.mahto77@gmail.com>
2021-01-25 17:18:14 +0530

    added all the files for spring boot registration login

---
+++ src/test/java/net/codejava/SpringBootRegistrationLoginApplicationTests.java
@@ -0,0 +1,15 @@
+package net.codejava;
+
+/*
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
